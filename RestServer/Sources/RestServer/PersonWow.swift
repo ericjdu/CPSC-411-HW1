c88b0852a36735e8f5ebe8232f72dc5969df981a
ericjdu <55962151+ericjdu@users.noreply.github.com>
2020-10-25 23:51:20 -0700

    Add files via upload

---
+++ RestServer/Sources/RestServer/PersonWow.swift
@@ -0,0 +1,61 @@
+//
+//  PersonWow.swift
+//  RestServer
+//
+//  Created by user923975 on 10/25/20.
+//
+
+import SQLite3
+import Foundation
+
+struct Claim : Codable {
+    var id : String
+    var title : String
+    var date : String
+    var isSolved = 0
+    
+    init(t : String, d : String, solv : Int?) {
+        id = UUID().uuidString
+        title = t
+        date = d
+        isSolved = 0
+    }
+}
+
+class PersonWow {
+   func addClaim(cObj : Claim) {
+        let sqlStmt = String(format:"insert into claim (tite, date) values ('%@', '%@')", (cObj.title), (cObj.date))
+        // get database connection
+        let conn = Database.getInstance().getDbConnection()
+        // submit the insert sql statement
+        if sqlite3_exec(conn, sqlStmt, nil, nil, nil) != SQLITE_OK {
+            let errcode = sqlite3_errcode(conn)
+            print("Failed to insert a Claim record due to error \(errcode)")
+        }
+        // close the connection
+        sqlite3_close(conn)
+    }
+    
+    func getAll() -> [Claim] {
+        var cList = [Claim]()
+        var resultSet : OpaquePointer?
+        let sqlStr = "select UUID, title, date, and isSolved from claim"
+        let conn = Database.getInstance().getDbConnection()
+        if sqlite3_prepare_v2(conn, sqlStr, -1, &resultSet, nil) == SQLITE_OK {
+            while(sqlite3_step(resultSet) == SQLITE_ROW) {
+                // Convert the record into a Person object
+                // Unsafe_Pointer<CChar> Sqlite3
+                let uid_val = sqlite3_column_text(resultSet, 0)
+                let uid = String(cString: uid_val!)
+                let title_val = sqlite3_column_text(resultSet, 1)
+                let title = String(cString: title_val!)
+                let date_val = sqlite3_column_text(resultSet, 2)
+                let date = String(cString: date_val!)
+                let isSolved_val = sqlite3_column_text(resultSet, 3)
+                let isSolved = isSolved_val
+                cList.append(Claim(uid:uid, t:title, d:date, isSolved:isSolved))
+            }
+        }
+        return pList
+    }
+}
---
+++ RestServer/Sources/RestServer/main.swift
@@ -0,0 +1,112 @@
+import Kitura
+import Cocoa
+import Foundation
+
+let router = Router()
+
+router.all("/PersonService/add", middleware: BodyParser())
+
+router.get("/"){
+    request, response, next in
+    response.send("Hello! Welcome to visit the service. ")
+    next()
+}
+/*
+router.post("/testPost"){
+    request, response, next in
+    print("Hitting the restPost service ...")
+    let body = request.body
+    if let jsonObj = body?.asJSON {
+        if let jDict = jsonObj as? [String : String] {
+            if let fn = jDict["FirstName"] {
+                print("The input First Name : \(fn)")
+                if let ln = jDict["LastName"] {
+                    print("The input Last Name : \(ln)")
+                }
+            }
+        }
+        
+    }
+    response.send("Hello! Welcome to visit the /testPost service. ")
+    next()
+}
+*/
+
+router.get("PersonService/getAll"){
+    request, response, next in
+    let pList = PersonDao().getAll()
+    // JSON Serialization
+    let jsonData : Data = try JSONEncoder().encode(pList)
+    //JSONArray 
+    let jsonStr = String(data: jsonData, encoding: .utf8)
+    // set Content-Type
+    response.status(.OK)
+    response.headers["Content-Type"] = "application/json"
+    response.send(jsonStr)
+    // response.send("getAll service response data : \(pList.description)")
+    next()
+}
+
+router.post("PersonService/add") {
+    request, response, next in
+    // JSON deserialization on Kitura server 
+    let body = request.body
+    let jObj = body?.asJSON //JSON object
+    if let jDict = jObj as? [String:String] {
+        if let fn = jDict["firstName"],let ln = jDict["lastName"],let n = jDict["ssn"] {
+            let pObj = Person(fn:fn, ln:ln, n:n)
+            PersonDao().addPerson(pObj: pObj)
+        }
+    }
+    response.send("The Person record was successfully inserted (via POST Method).")
+    next()
+}
+router.get("/PersonService/add") {
+    request, response, next in
+    let fn = request.queryParameters["FirstName"]
+    let ln = request.queryParameters["LastName"]
+    //
+    // let n = ....
+    // if n != nil {
+    // ... }
+    if let n = request.queryParameters["SSN"] {
+        let pObj = Person(fn:fn, ln:ln, n:n)
+        PersonDao().addPerson(pObj: pObj)
+        response.send("The Person record was successfully inserted.")
+    } else {
+        
+    }
+    next() 
+}
+
+router.post("/ClaimService/add") {
+    request, response, next in
+    // JSON deserialization on Kitura server
+    let body = request.body
+    let jObj = body?.asJSON //JSON object
+    if let jDict = jObj as? [String:String] {
+        if let fn = jDict["title"],let ln = jDict["date"]{
+            let cObj = Claim(title:title, date:date)
+            PersonWow().addClaim(cObj: cObj)
+        }
+    }
+    response.send("The Person record was successfully inserted (via POST Method).")
+    next()}
+
+router.get("/ClaimService/getAll") {
+    request, response, next in
+    let cList = PersonWow().getAll()
+    // JSON Serialization
+    let jsonData : Data = try JSONEncoder().encode(pList)
+    //JSONArray
+    let jsonStr = String(data: jsonData, encoding: .utf8)
+    // set Content-Type
+    response.status(.OK)
+    response.headers["Content-Type"] = "application/json"
+    response.send(jsonStr)
+    // response.send("getAll service response data : \(pList.description)")
+    next()}
+
+Kitura.addHTTPServer(onPort: 8020, with: router)
+Kitura.run()
+
